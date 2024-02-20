d8363eb364321340b1d8d5b7c387675301eadf42
Gabriel Barbosa <gabrielbarbosaa432@gmail.com>
2024-02-20 17:15:38 -0300

    feat: initial config

---
+++ src/server/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const server = express();
+//Toda config do servidor **
+
+//Teste para vê se o 'servidor' está funcionando
+server.get('/', (req, res ) => {
+    return res.send('Olá dev')
+})
+
+
+// Exportando o servidor para uso em outros arquivos
+export { server }
