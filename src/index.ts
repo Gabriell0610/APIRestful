26e1843255bfd165940138f833ea345f38fa1bf2
Gabriel Barbosa <gabrielbarbosaa432@gmail.com>
2024-02-20 18:45:44 -0300

    feat: add code style configs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-import {server} from './server/server';
-
+import { server } from "./server/server";
 
 server.listen(3333, () => {
-    console.log('App rodando')
-})
+  console.log("App rodando");
+});
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,13 +1,13 @@
-import express from 'express';
+/* eslint-disable linebreak-style */
+import express from "express";
 
 const server = express();
 //Toda config do servidor **
 
 //Teste para vê se o 'servidor' está funcionando
-server.get('/', (req, res ) => {
-    return res.send('Olá dev')
-})
-
+server.get("/", (req, res) => {
+  return res.send("Olá dev");
+});
 
 // Exportando o servidor para uso em outros arquivos
-export { server }
+export { server };
