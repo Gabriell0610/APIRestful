0bfba797ab4adf3ec16e46fe7b0e7f95736fb549
Gabriel Barbosa <gabrielbarbosaa432@gmail.com>
2024-02-27 23:51:58 -0300

    add http-status-codes

---
+++ src/server/routes/index.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { StatusCodes } from 'http-status-codes'
+
+const router = Router();
+
+//Para que todas as rotas definidas em routes.ts sejam acessíveis a partir do caminho raiz "/",
+//é precisa passar "/" como o primeiro argumento para server.use():
+router.get("/", (req, res) => {
+  return res.send("Olá dev");
+});
+
+router.post("/teste", (req, res) => {
+  console.log(req.body);
+
+  return res.status(StatusCodes.).json(req.body);
+});
+
+export { router };
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,13 +1,14 @@
 /* eslint-disable linebreak-style */
 import express from "express";
+import { router } from "./routes/";
 
 const server = express();
 //Toda config do servidor **
 
-//Teste para vê se o 'servidor' está funcionando
-server.get("/", (req, res) => {
-  return res.send("Olá dev");
-});
+// Usando um middleware para poder acessar a solicitação do usuário dentro da rota
+server.use(express.json());
+
+server.use("/", router);
 
 // Exportando o servidor para uso em outros arquivos
 export { server };
